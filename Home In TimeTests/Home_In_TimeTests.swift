5908cef8aa6ad05f3276147689050aa350f6a399
Brady <Brady@todd-stoffels-imac.local>
2015-06-30 10:41:58 -0500

    Initial Commit

---
+++ Home In TimeTests/Home_In_TimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Home_In_TimeTests.swift
+//  Home In TimeTests
+//
+//  Created by Brady Stoffel on 6/30/15.
+//  Copyright (c) 2015 Brady Stoffel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Home_In_TimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
